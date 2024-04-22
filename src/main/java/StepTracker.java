0aaa1e0cf5d56a898d36de8e69e91af162dcf60f
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-04-22 12:56:22 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,27 +1,24 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- private int numActiveDays;
- private int goalSteps;
- private ArrayList <Integer> dailySteps;
+ private int numActiveDays, goalSteps, numDays;
  public StepTracker(int goal){
  goalSteps = goal;
  numActiveDays = 0;
- dailySteps = new ArrayList <Integer>();
+ numDays = 0;
  }
  public void addDailySteps(int numSteps){
+  numDays++;
   if(numSteps >= goalSteps)
    numActiveDays++;
-   dailySteps.add(numSteps);
  }
  public int activeDays(){
   return numActiveDays;
  }
  public double averageSteps(){
-  double sumSteps = 0;
-  for(int i = 0; i < dailySteps.size(); i++)
-   sumSteps = sumSteps + dailySteps.get(i);
-  double averageStep = sumSteps/dailySteps.size();
+  if(numActiveDays == 0)
+    return 0.0;
+  double averageStep = numActiveDays/numDays;
   return averageStep;
  }
 } 
